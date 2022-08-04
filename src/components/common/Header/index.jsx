3fd0997ddec2b110f58cfe3903cc0ab63e6a7444
Danilo Valois <danilo.nvr@gmail.com>
2022-08-03 22:30:21 -0300

    finalização pagina home/ mobile-tablet

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -6,13 +6,14 @@ import logo from "../../../assets/logo-caseiros.png"
 function Header() {
   return (
     <>
+    <div className="header-blur"></div>
     <header className='header'>
         <img src={logo} alt="logo"/>
         <div className="header-text">
         <span>Bem-Vindo</span>
         </div>
         <a target="blank" href='https://api.whatsapp.com/send?phone=5561996045544&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20pedido!'>
-        <ImWhatsapp size={30} color="#fff"/>
+        <ImWhatsapp size={30} color="rgba(0, 173, 111, 1)"/>
         </a>
     </header>
     </>
